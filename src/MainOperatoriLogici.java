204941b1ad59db2f3a8a69dc02fdf5cab4200e2e
Davide Corsi <davide.corsi@par-tec.it>
2021-09-30 17:31:01 +0200

    first commit

---
+++ src/MainOperatoriLogici.java
@@ -0,0 +1,12 @@
+
+public class MainOperatoriLogici {
+	public static void main(String[] args) {
+		int eta = 5;
+		
+		if(eta > 18 && eta < 65) {
+			System.out.println("Sei un adulto");
+		} else {
+			System.out.println("Sei giovanissimo o molto avanti con l'età");
+		}
+	}
+}
