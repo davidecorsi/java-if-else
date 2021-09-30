43b943517bf5fb5cc0c0960dd02058ae4166ac5d
Davide Corsi <davide.corsi@par-tec.it>
2021-09-30 16:55:52 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int eta = 22;
+		
+		if(eta > 18) {
+			System.out.println("Hai più di 18 anni");
+		} else {
+			System.out.println("Non hai ancora più di 18 anni");
+		}
+	}
+}
